4b0625956c2588f219c6be5215dde0e649902388
David Petryca <david@petryca.com>
2022-05-17 08:56:39 +0200

    add regex to insert nbsp

--- app.js
+++ app.js
@@ -3,12 +3,35 @@
     let response = await fetch('data.json');
     let data = await response.json(); 
 
+    // https://translations.ted.com/How_to_break_lines
+
+    function nobsp(str) {
+        let searchfor = ['a', 'an', 'and', 'the', 'of', 'in', 'at', 'to', 'on', 'or', 'as', 'so'];
+
+        str = str.replace(/\s(\w+[\.]?)$/, "&nbsp$1");
+
+        for (let i = 0; i < searchfor.length; i++) {
+            let find = ' ' + searchfor[i] + ' ';
+            let replace = ' ' + searchfor[i] + '&nbsp;';
+            str = str.replaceAll(find, replace);
+            find = '&nbsp;' + searchfor[i] + ' ';
+            replace = ' ' + searchfor[i] + '&nbsp;'
+            str = str.replaceAll(find, replace);
+        }
+
+        return str;
+    }
+
     document.querySelector('button').onclick = function () {
         document.querySelector('blockquote').classList.add('hidden');
 
         setTimeout(() => {
             var quote = data[Math.floor(Math.random() * data.length)];
-            document.querySelector('p').innerText = quote.text;
+
+            console.log(quote.text);
+            console.log(nobsp(quote.text));
+
+            document.querySelector('p').innerHTML = nobsp(quote.text);
             document.querySelector('a').innerText = quote.book + ' by ' + quote.author;
             document.querySelector('a').setAttribute('href', quote.url);
             document.querySelector('blockquote').classList.remove('hidden');
